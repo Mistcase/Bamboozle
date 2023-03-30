bcbab7658314f5083a9abb59d420e0e32bc35699
Ivan Khramtsou <ivan.khramtsou@gmail.com>
2023-03-30 20:22:23 +0300

    Support for new entity creation (Editor).

--- Sandbox/src/SceneUITools.h
+++ Sandbox/src/SceneUITools.h
@@ -19,8 +19,16 @@ namespace butterfly
         void drawComponentsPanel();
         void drawStatusPanel();
 
+        void showSetDirectionPopup(DirectionalLightComponent& component);
+        void showNewEntityPopup();
+
     private:
         Ref<Scene> m_scene;
         Entity m_selected;
+
+    private:
+        static const size_t MaxStringLength = 64;
+        glm::vec3 m_newLightDirection;
+        char m_newEntityName[MaxStringLength];
     };
 } // namespace butterfly
